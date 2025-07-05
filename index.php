d09b1c7a0a2e6604623472015c6afa2d743513d4
davenpos <davenpos@uwindsor.ca>
2025-07-05 16:08:04 -0400

    Added trail map image to Report Page

--- index.php
+++ index.php
@@ -3,5 +3,10 @@ while (have_posts()):
   the_post(); ?>
   <h1><?php the_title(); ?></h1>
   <?php the_content();
+  if (get_the_ID() === 18): ?>
+    <div id="imageContainer">
+      <img src="<?php echo wp_get_attachment_image_src(6, 'medium_large')[0]; ?>" />
+    </div>
+  <?php endif;
 endwhile;
-get_footer(); ?>
+get_footer();
