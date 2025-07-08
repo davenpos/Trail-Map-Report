3683f64c28d1335e50a1f08318ade496e5d50f97
davenpos <davenpos@uwindsor.ca>
2025-07-08 11:09:25 -0400

    Removed enqueue style for Snowburst font because I'm not using that font on the site anymore

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
 add_action("wp_enqueue_scripts", function () {
   wp_enqueue_style("main-css", get_theme_file_uri("/build/style-index.css"), [], filemtime(get_theme_file_path("/build/style-index.css")));
-  wp_enqueue_style("snowburst-font", "//fonts.googleapis.com/css2?family=Snowburst+One&display=swap");
   wp_enqueue_script("main-js", get_theme_file_uri("/build/index.js"), array("jquery"), false, true);
   wp_localize_script("main-js", "siteData", array(
     "trailData" => get_option("latest_report_data")
