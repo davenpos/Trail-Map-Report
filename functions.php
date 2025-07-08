9cdec9c289ad3814fcdca37ba65467cbe3b1b2dd
davenpos <davenpos@uwindsor.ca>
2025-07-08 11:04:55 -0400

    Had last modified time be version number to ensure browser fetches updated version without needing to hard refresh

--- functions.php
+++ functions.php
@@ -1,18 +1,18 @@
 <?php
 add_action("wp_enqueue_scripts", function () {
-  wp_enqueue_style("main-css", get_theme_file_uri("/build/style-index.css"));
+  wp_enqueue_style("main-css", get_theme_file_uri("/build/style-index.css"), [], filemtime(get_theme_file_path("/build/style-index.css")));
   wp_enqueue_style("snowburst-font", "//fonts.googleapis.com/css2?family=Snowburst+One&display=swap");
   wp_enqueue_script("main-js", get_theme_file_uri("/build/index.js"), array("jquery"), false, true);
   wp_localize_script("main-js", "siteData", array(
     "trailData" => get_option("latest_report_data")
   ));
   if (is_page(8)) {
-    wp_enqueue_style("form-css", get_theme_file_uri("/build/css/form.css"));
-    wp_enqueue_style("mountains-css", get_theme_file_uri("/build/css/mountains.css"));
-    wp_enqueue_style("cloud-css", get_theme_file_uri("/build/css/cloud.css"));
+    wp_enqueue_style("form-css", get_theme_file_uri("/build/css/form.css"), [], filemtime(get_theme_file_path("/build/css/form.css")));
+    wp_enqueue_style("mountains-css", get_theme_file_uri("/build/css/mountains.css"), [], filemtime(get_theme_file_path("/build/css/mountains.css")));
+    wp_enqueue_style("cloud-css", get_theme_file_uri("/build/css/cloud.css"), [], filemtime(get_theme_file_path("/build/css/cloud.css")));
   }
   if (is_page(18)) {
-    wp_enqueue_style("image-map-css", get_theme_file_uri("/build/css/imagemap.css"));
+    wp_enqueue_style("image-map-css", get_theme_file_uri("/build/css/imagemap.css"), [], filemtime(get_theme_file_path("/build/css/imagemap.css")));
   }
 });
 
